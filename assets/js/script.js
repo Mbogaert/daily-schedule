9b9ad4a9754b5974d9f62c3cb36bafe056d7ecbb
Mathew B <bogaert@email.arizona.edu>
2020-10-25 13:42:16 -0700

    displaying date at the top

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,21 @@
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
 
+// THEN the current day is displayed at the top of the calendar
+function displayDate() {
+    // display current day at the top of the calendar
+    var setDate = moment().format("dddd, MMMM Do, YYYY");
+    var $currentDay = $("#currentDay");
+  
+    $currentDay.text(setDate); 
+}
 
 // WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
+// THEN I am presented with timeblocks for standard business hours - HTML grid
 
 
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// WHEN I view the timeblocks for that day 
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future - red, yellow, green bootstrap
 
 
 // WHEN I click into a timeblock
@@ -19,5 +26,7 @@
 // THEN the text for that event is saved in local storage
 
 
-// WHEN I refresh the page
+// WHEN I refresh the page - localStorage
 // THEN the saved events persist
+
+displayDate();
