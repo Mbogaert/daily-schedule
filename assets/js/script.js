57bfa152983f9b7e66d9b1a402ab3f7f5a063585
Mathew B <bogaert@email.arizona.edu>
2020-10-25 20:29:13 -0700

    final touches, cleaned up some words

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,35 +46,35 @@ $(document).ready(function () {
     // THEN the text for that event is saved in local storage
     $("button").on("click", function () {
         localStorage.setItem("9am", JSON.stringify($9.val()));
-        localStorage.setItem("10AM", JSON.stringify($10.val()));
-        localStorage.setItem("11AM", JSON.stringify($11.val()));
-        localStorage.setItem("12PM", JSON.stringify($12.val()));
-        localStorage.setItem("1PM", JSON.stringify($13.val()));
-        localStorage.setItem("2PM", JSON.stringify($14.val()));
-        localStorage.setItem("3PM", JSON.stringify($15.val()));
-        localStorage.setItem("4PM", JSON.stringify($16.val()));
-        localStorage.setItem("5PM", JSON.stringify($17.val()));
+        localStorage.setItem("10am", JSON.stringify($10.val()));
+        localStorage.setItem("11am", JSON.stringify($11.val()));
+        localStorage.setItem("12pm", JSON.stringify($12.val()));
+        localStorage.setItem("1pm", JSON.stringify($13.val()));
+        localStorage.setItem("2pm", JSON.stringify($14.val()));
+        localStorage.setItem("3pm", JSON.stringify($15.val()));
+        localStorage.setItem("4pm", JSON.stringify($16.val()));
+        localStorage.setItem("5pm", JSON.stringify($17.val()));
     });
 
     // WHEN I refresh the page
     // THEN the saved events persist
     var persist9 = JSON.parse(localStorage.getItem("9am"));
     $("#9").val(persist9);
-    var persist10 = JSON.parse(localStorage.getItem("10AM"));
+    var persist10 = JSON.parse(localStorage.getItem("10am"));
     $("#10").val(persist10);
-    var persist11 = JSON.parse(localStorage.getItem("11AM"));
+    var persist11 = JSON.parse(localStorage.getItem("11am"));
     $("#11").val(persist11);
-    var persist12 = JSON.parse(localStorage.getItem("12PM"));
+    var persist12 = JSON.parse(localStorage.getItem("12pm"));
     $("#12").val(persist12);
-    var persist13 = JSON.parse(localStorage.getItem("1PM"));
+    var persist13 = JSON.parse(localStorage.getItem("1pm"));
     $("#13").val(persist13);
-    var persist14 = JSON.parse(localStorage.getItem("2PM"));
+    var persist14 = JSON.parse(localStorage.getItem("2pm"));
     $("#14").val(persist14);
-    var persist15 = JSON.parse(localStorage.getItem("3PM"));
+    var persist15 = JSON.parse(localStorage.getItem("3pm"));
     $("#15").val(persist15);
-    var persist16 = JSON.parse(localStorage.getItem("4PM"));
+    var persist16 = JSON.parse(localStorage.getItem("4pm"));
     $("#16").val(persist16);
-    var persist17 = JSON.parse(localStorage.getItem("5PM"));
+    var persist17 = JSON.parse(localStorage.getItem("5pm"));
     $("#17").val(persist17);
 
 });
